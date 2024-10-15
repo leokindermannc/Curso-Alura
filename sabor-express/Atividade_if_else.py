6aa1e652f026bdbf2b29991c847a066b5a350da8
Leonardo Kindermann Campos <kindermanncampos13@gmail.com>
2024-10-15 17:21:18 -0300

    Add files via upload

---
+++ sabor-express/Atividade_if_else.py
@@ -0,0 +1,77 @@
+#def entrada_de_dados():
+    
+    #numero=int(input('Informe um numero:'))
+
+
+    #if numero % 2==0:
+        #print(f'{numero} e par')
+    #else:
+        #print(f'{numero} e impar')
+
+
+
+
+
+
+
+#def entrada_de_dados()
+    #idade=int(input('Digite sua idade'))
+    #if 0>=idade<=12:
+       # print('Crianca')
+    #elif 13>=idade<=18:
+       # print('Adolescente')
+    #else:
+        #print('Adulto')
+    
+
+#def main():
+    #entrada_de_dados()
+
+#if __name__=='__main__':
+    #main()
+
+
+#usuario_esperado='leokindermannc'
+#senha_esperada='fofinho13'
+#nome=(input('nome de usuario'))
+#senha=(input('Digite senha'))
+#if nome==usuario_esperado and senha==senha_esperada:
+       # print('Login acessado com sucesso!')
+#else:
+   # print('Login incorreto!')
+
+
+
+#x=int(input('Informe o valor de X'))
+y=int(input('Informe valor de y'))
+
+if  x>0 and y>0:
+    print('Primeiro Quadrante')
+
+elif x<0 and y>0:
+    print('Segundo Quadrante')
+
+elif x<0 and y<0:
+    print('Terceito Quadrante')
+
+elif x>0 and y<0:
+    print('Quarto Quadrante')
+else:
+    print('O ponto esta localizado no eixo ou origem.')
+
+
+
+
+
+
+
+   
+   
+   
+   
+
+
+
+        
+
+
